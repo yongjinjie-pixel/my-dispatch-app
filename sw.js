168ec94deb57b95e4d2d9f77c242c82492ed7f8a
yongjinjie-pixel <yongjinjie@gmail.com>
2026-07-10 22:45:02 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,31 @@
-const CACHE = "dispatch-pilot-v1";
-const FILES = ["./", "./index.html", "./styles.css", "./app.js", "./scheduler.js", "./manifest.json"];
-self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(FILES))));
-self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request))));
+const CACHE_NAME = 'dispatch-pilot-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.json'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  console.log('Service Worker activated');
+});
+
+// Fetch Event (Mandatory requirement for Chrome installation)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
